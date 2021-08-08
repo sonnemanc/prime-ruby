7b59b6bdf91242d3dadee0f06db76514c34bbe3f
Corban <sonnemanc@gmail.com>
2021-08-08 16:44:27 -0600

    Done with Assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime?(n)
+  return false if !n.integer? 
+  return false if n < 2
+  return true if n == 2
+  (2..n-1).each {|numb| return false if n % numb == 0}
+  true
+end
